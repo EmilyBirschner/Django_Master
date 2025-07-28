c1dbc8fc8a290eca1b09892b6b9169d2be7126a1
Emily <emilyb.oliveira@hotmail.com>
2025-07-28 12:15:36 -0300

    Exemplos de operadores lógicos

---
+++ Exemplos_op_logicos/op_logicos.py
@@ -0,0 +1,21 @@
+a = True
+b = False
+
+# and, or
+
+if a and b:
+    print("Atendeu a condição!")
+else:
+    print("Não atendeu a condição!")
+
+
+nome = "Felipe"
+idade = 25
+
+# ==, !=, >, <, <=, >=
+
+# if idade == 25:
+if nome == "Felipe" and idade == 25:
+    print("Dados corretos!")
+else:
+    print("Dados incorretos!")
